af2d2de237238489cad8549323cf3771f60cc005
violeroares <violeroares@hotmail.com>
2025-06-04 22:45:21 -0300

    new - crear modelo de post

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/post/models/Post.kt
@@ -0,0 +1,14 @@
+package ar.edu.unlam.mobile.scaffolding.domain.post.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Post(
+    val id: Int,
+    val message: String,
+    @SerializedName("parent_id") val parentId: Int,
+    val author: String,
+    @SerializedName("avatar_url") val avatarUrl: String,
+    val likes: Int,
+    val liked: Boolean,
+    val date: String
+)

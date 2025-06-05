c075af7355d6b854ef4406a1b5802e94dd2f80ed
violeroares <violeroares@hotmail.com>
2025-06-04 22:51:36 -0300

    new - agregado de API_KEY en build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,7 @@
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -23,6 +27,16 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        val properties = Properties()
+        val localProperties = File(rootProject.rootDir, "local.properties")
+        if (localProperties.exists()) {
+            properties.load(FileInputStream(localProperties))
+        }
+        buildConfigField(
+            "String",
+            "API_KEY",
+            "\"${properties.getProperty("API_KEY", "")}\"",
+        )
     }
 
     buildTypes {
@@ -43,6 +57,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
     packaging {
         resources {

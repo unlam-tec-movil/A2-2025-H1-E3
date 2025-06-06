e8142ff5720502ff89236dd99433537f311d055a
violeroares <violeroares@hotmail.com>
2025-06-06 02:06:58 -0300

    fix - limpieza de código

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/HomeViewModel.kt
@@ -35,7 +35,16 @@ class HomeViewModel
     constructor(
         private val postService: PostService,
     ) : ViewModel() {
+        // Mutable State Flow contiene un objeto de estado mutable. Simplifica la operación de
+        // actualización de información y de manejo de estados de una aplicación: Cargando, Error, Éxito
+        // (https://developer.android.com/kotlin/flow/stateflow-and-sharedflow)
+        // _helloMessage State es el estado del componente "HelloMessage" inicializado como "Cargando"
+
+        // _Ui State es el estado general del view model.
         private val _uiState = MutableStateFlow(PostUIState(FeedUIState.Loading))
+
+        // UIState expone el estado anterior como un Flujo de Estado de solo lectura.
+        // Esto impide que se pueda modificar el estado desde fuera del ViewModel.
         val uiState = _uiState.asStateFlow()
 
         init {

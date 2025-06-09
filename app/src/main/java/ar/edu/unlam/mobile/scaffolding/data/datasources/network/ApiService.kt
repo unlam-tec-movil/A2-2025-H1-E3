2071068baaeb6023da0584d36a06778705f6d5d4
Nataliaemucci <88558558+Nataliaemucci@users.noreply.github.com>
2025-06-09 16:19:17 -0300

    new - creatPost en apiService

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/datasources/network/ApiService.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/datasources/network/ApiService.kt
@@ -1,9 +1,17 @@
 package ar.edu.unlam.mobile.scaffolding.data.datasources.network
 
 import ar.edu.unlam.mobile.scaffolding.domain.post.models.Post
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("api/v1/me/feed")
     suspend fun getFeed(): List<Post>
+
+    @POST("/api/v1/me/tuits")
+    suspend fun createPost(
+        @Body request: Post,
+    ): Response<Unit>
 }

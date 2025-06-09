45c72068dc596dd4f71f1c4cb28fb5aee9a70bfd
sadtoru <daiarocio@outlook.com>
2025-06-09 18:59:27 -0300

    implementación inicial de LoginUseCase

---
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/user/usecases/LoginUseCase.kt
@@ -0,0 +1,17 @@
+package ar.edu.unlam.mobile.scaffolding.domain.user.usecases
+
+import ar.edu.unlam.mobile.scaffolding.data.datasources.local.AuthToken
+import ar.edu.unlam.mobile.scaffolding.domain.user.repository.IUserRepository
+
+class LoginUseCase(
+    private val userRepository: IUserRepository,
+    private val authToken: AuthToken,
+) {
+    suspend operator fun invoke(
+        email: String,
+        password: String,
+    ) {
+        val token = userRepository.login(email, password)
+        authToken.userToken = token
+    }
+}

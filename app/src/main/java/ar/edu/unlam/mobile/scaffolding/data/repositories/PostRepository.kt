3e4e7a7e2a7af676e483562b3936cb3fa255c536
Nataliaemucci <88558558+Nataliaemucci@users.noreply.github.com>
2025-06-09 16:19:59 -0300

    new - creatPost en PostRepository

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repositories/PostRepository.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/data/repositories/PostRepository.kt
@@ -3,6 +3,7 @@ package ar.edu.unlam.mobile.scaffolding.data.repositories
 import ar.edu.unlam.mobile.scaffolding.data.datasources.network.ApiService
 import ar.edu.unlam.mobile.scaffolding.domain.post.models.Post
 import ar.edu.unlam.mobile.scaffolding.domain.post.repository.IPostRepository
+import coil.network.HttpException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -19,4 +20,18 @@ class PostRepository(
             val response = apiService.getFeed()
             emit(response)
         }.flowOn(Dispatchers.IO)
+
+    override suspend fun createPost(post: Post) {
+        try {
+            val response = apiService.createPost(post)
+
+            if (!response.isSuccessful) {
+                throw Exception(response.code().toString())
+            }
+        } catch (e: HttpException) {
+            throw e
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 }

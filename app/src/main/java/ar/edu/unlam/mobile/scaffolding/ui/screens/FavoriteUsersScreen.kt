1bd5fefd50b017d7ef2de0f781fe7dc7a2741a45
Gonzalo <gnizzi1990@gmail.com>
2025-06-15 18:17:34 -0300

    prueba de textos

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/FavoriteUsersScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/FavoriteUsersScreen.kt
@@ -4,23 +4,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 
 @Composable
-fun FavUsersScreen(
-    navController: NavController,
-    viewModel: FavoriteUsersViewModel = hiltViewModel()
-) {
-    Column (
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Center
-    ){
+fun favUsersScreen(text: String, viewModel: FavoriteUsersViewModel = hiltViewModel()) {
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
 
+    ) {
+        Text(text)
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun UserScreenPreview() {
+    favUsersScreen("TITULO PRUEBA")
 }

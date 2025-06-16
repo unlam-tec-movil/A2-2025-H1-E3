ace1680d9ef417c0cc9d0dba9a6a71ac65b1cc0c
Gonzalo <gnizzi1990@gmail.com>
2025-06-15 23:56:43 -0300

    barras top y bottom

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/FavoriteUsersScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/FavoriteUsersScreen.kt
@@ -3,32 +3,69 @@ package ar.edu.unlam.mobile.scaffolding.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun favUsersScreen(text: String, viewModel: FavoriteUsersViewModel = hiltViewModel()) {
-    Column(
-        modifier =
-            Modifier
-                .fillMaxSize()
-                .padding(24.dp),
-        verticalArrangement = Arrangement.Center,
-
-    ) {
-        Text(text)
+fun FavUsersScreen(text: String, viewModel: FavoriteUsersViewModel = hiltViewModel()) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Usuarios Favoritos")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "BARRA BAJA"
+                )
+            }
+        }
+    ){innerPadding ->
+        Column(
+            modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            ){
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = ""
+            )
+        }
     }
 }
+
+
 
 @Preview(showSystemUi = true)
 @Composable
 fun UserScreenPreview() {
-    favUsersScreen("TITULO PRUEBA")
+    FavUsersScreen("TITULO PRUEBA")
 }

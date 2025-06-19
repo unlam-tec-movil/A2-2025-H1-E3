9b77abf5a46d20ae93fc1bbc9650dccda4d4e114
sadtoru <daiarocio@outlook.com>
2025-06-19 17:34:25 -0300

    feat - edit user

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/UserScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/UserScreen.kt
@@ -1,5 +1,6 @@
 package ar.edu.unlam.mobile.scaffolding.ui.screens
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,19 +11,25 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import ar.edu.unlam.mobile.scaffolding.ui.components.Avatar
 import ar.edu.unlam.mobile.scaffolding.ui.components.CustomHeader
 import ar.edu.unlam.mobile.scaffolding.ui.components.Greeting
+import ar.edu.unlam.mobile.scaffolding.ui.components.ProfileUserForm
+import ar.edu.unlam.mobile.scaffolding.ui.components.StateButton
 
 @Composable
 fun UserScreen(
-    userSessionViewModel: UserSessionViewModel = hiltViewModel(),
+    // userSessionViewModel: UserSessionViewModel = hiltViewModel(),
+    userViewModel: UserViewModel = hiltViewModel(),
     navController: NavController,
 ) {
-    val user by userSessionViewModel.user.collectAsState()
+    // val user by userSessionViewModel.user.collectAsState()
+    val uiState by userViewModel.uiState.collectAsState()
+    val context = LocalContext.current
 
     Scaffold(
         topBar = {
@@ -38,12 +45,36 @@ fun UserScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Avatar(
-                avatarUrl = user?.avatarUrl,
+                avatarUrl = uiState.avatarUrl,
                 avatarSize = 110.dp,
             )
 
             Spacer(modifier = Modifier.padding(top = 16.dp))
-            Greeting(user?.name ?: "")
+            Greeting("Hola! ${ uiState.name }")
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            ProfileUserForm(
+                header = "Editar perfil",
+                isEditing = true,
+                name = uiState.name,
+                avatarUrl = uiState.avatarUrl,
+                password = uiState.password,
+                confirmPassword = uiState.confirmPassword,
+                nameError = uiState.nameError,
+                passwordError = uiState.passwordError,
+                confirmPasswordError = uiState.confirmPasswordError,
+                onNameChange = { userViewModel.onNameChange(it) },
+                onPasswordChange = { userViewModel.onPasswordChange(it) },
+                onConfirmPasswordChange = { userViewModel.onConfirmPasswordChange(it) },
+                onEmailChange = {},
+                onAvatarUrlChange = { userViewModel.onAvatarUrlChange(it) },
+            )
+            Spacer(modifier = Modifier.padding(top = 16.dp))
+            StateButton(text = "Guardar cambios", isLoading = uiState.isLoading, onClick = {
+                userViewModel.editUser(
+                    onEditSuccess = { navController.popBackStack() },
+                    onError = { Toast.makeText(context, it, Toast.LENGTH_LONG).show() },
+                )
+            })
         }
     }
 }
--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/UserViewModel.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/UserViewModel.kt
@@ -1,10 +1,118 @@
 package ar.edu.unlam.mobile.scaffolding.ui.screens
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ar.edu.unlam.mobile.scaffolding.domain.user.usecases.GetCachedUserUseCase
+import ar.edu.unlam.mobile.scaffolding.domain.user.usecases.UpdateUserProfileUseCase
+import ar.edu.unlam.mobile.scaffolding.utils.ValidationUtils
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class EditUserState(
+    val name: String = "",
+    val avatarUrl: String = "",
+    val password: String = "",
+    val confirmPassword: String = "",
+    val isLoading: Boolean = false,
+    val nameError: String? = null,
+    val avatarUrlError: String? = null,
+    val passwordError: String? = null,
+    val confirmPasswordError: String? = null,
+    val showErrors: Boolean = false,
+)
 
 @HiltViewModel
 class UserViewModel
     @Inject
-    constructor() : ViewModel()
+    constructor(
+        private val getCachedUserUseCase: GetCachedUserUseCase,
+        private val updateUserProfileUseCase: UpdateUserProfileUseCase,
+    ) : ViewModel() {
+        private val _uiState = MutableStateFlow(EditUserState())
+        val uiState: StateFlow<EditUserState> = _uiState
+
+        init {
+            loadUserData()
+        }
+
+        private fun loadUserData() {
+            viewModelScope.launch {
+                val user = getCachedUserUseCase()
+                _uiState.value =
+                    EditUserState(
+                        name = user?.name.orEmpty(),
+                        avatarUrl = user?.avatarUrl.orEmpty(),
+                    )
+            }
+        }
+
+        fun onNameChange(name: String) {
+            _uiState.update { it.copy(name = name) }
+        }
+
+        fun onPasswordChange(password: String) {
+            _uiState.update { it.copy(password = password) }
+        }
+
+        fun onConfirmPasswordChange(confirmPassword: String) {
+            _uiState.update { it.copy(confirmPassword = confirmPassword) }
+        }
+
+        fun onAvatarUrlChange(avatarUrl: String) {
+            _uiState.update { it.copy(avatarUrl = avatarUrl) }
+        }
+
+        private fun setLoading(isLoading: Boolean) {
+            _uiState.update { it.copy(isLoading = isLoading) }
+        }
+
+        private fun verifyEditUser() {
+            val nameValid = ValidationUtils.isNameValid(uiState.value.name)
+            val passwordValid = ValidationUtils.isPasswordValid(uiState.value.password)
+            val confirmPasswordValid =
+                ValidationUtils.doPasswordsMatch(uiState.value.password, uiState.value.confirmPassword)
+            val avatarUrlValid = (uiState.value.avatarUrl.isEmpty())
+            _uiState.update {
+                it.copy(
+                    nameError = if (!nameValid) "El nombre debe tener al menos 3 caracteres" else null,
+                    passwordError = if (!passwordValid) "La contraseña debe tener al menos 3 caracteres" else null,
+                    confirmPasswordError = if (!confirmPasswordValid) "Las contraseñas no coinciden" else null,
+                    avatarUrlError = if (!avatarUrlValid) "El campo no puede estar vacio" else null,
+                )
+            }
+        }
+
+        fun editUser(
+            onEditSuccess: () -> Unit,
+            onError: (String) -> Unit,
+        ) {
+            _uiState.update { it.copy(showErrors = true) }
+            verifyEditUser()
+            if (_uiState.value.nameError != null ||
+                _uiState.value.passwordError != null ||
+                _uiState.value.confirmPasswordError != null ||
+                _uiState.value.avatarUrlError != null
+            ) {
+                return
+            }
+            viewModelScope.launch {
+                setLoading(true)
+                try {
+                    updateUserProfileUseCase(
+                        _uiState.value.name,
+                        _uiState.value.avatarUrl,
+                        _uiState.value.password,
+                    )
+                } catch (e: Exception) {
+                    onError(e.message ?: "Error al editar el usuario")
+                } finally {
+                    setLoading(false)
+                    onEditSuccess()
+                }
+            }
+        }
+    }

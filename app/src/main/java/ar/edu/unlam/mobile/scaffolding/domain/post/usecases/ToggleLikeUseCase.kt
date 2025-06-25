43e2a9ec604f4236fc81418b17449613340456b4
violeroares <violeroares@hotmail.com>
2025-06-25 04:32:51 -0300

    new - refactorización de sessionManager

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/post/usecases/ToggleLikeUseCase.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/domain/post/usecases/ToggleLikeUseCase.kt
@@ -1,18 +1,19 @@
 package ar.edu.unlam.mobile.scaffolding.domain.post.usecases
 
+import ar.edu.unlam.mobile.scaffolding.domain.post.models.Post
 import ar.edu.unlam.mobile.scaffolding.domain.post.repository.IPostRepository
+import kotlinx.coroutines.flow.Flow
 
 class ToggleLikeUseCase(
     private val postRepository: IPostRepository,
 ) {
-    suspend operator fun invoke(
+    operator fun invoke(
         postId: String,
         like: Boolean,
-    ) {
+    ): Flow<Post> =
         if (like) {
             postRepository.addLike(postId)
         } else {
             postRepository.removeLike(postId)
         }
-    }
 }
